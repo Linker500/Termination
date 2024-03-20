8b041760928cb80902544c310a4c13984646b00a
linker <github@linkerlabs.net>
2024-03-20 19:49:54 -0400

    Cleanup, reorganizaiton, misc additions

--- Termination/Screen.cs
+++ Termination/Screen.cs
@@ -11,38 +11,22 @@ using Microsoft.VisualBasic;
  * They contain a coordinate position, a length, height, and then display data within it.
  */
 
-
-// render loop thread
-// Detect resolution change → call rerender
-// 
-
 public class Screen
 {
+    public ConsoleColor FGColor {get; set;} //
+    public ConsoleColor BGColor {get; set;} //
+
     public readonly Dictionary<string,Window> Windows = new(); //Dictionary of windows for this Screen
-    public int SizeX, SizeY; //Size of this screen
+    //public int SizeX, SizeY; //Size of this screen
     
     public Screen()
     {
-        (SizeX, SizeY) = TermInfo.GetSize();
-        Windows.Add("one",new TextBlock(0.0, 0.0, 0.3, 0.4));
-        Windows.Add("two",new TextBlock(0.05, 0.4, 0.6, 0.6));
-        Windows.Add("thr",new TextBlock(0.4, 0.04, 0.6, 0.35));
-        //Windows.Add("fou",new TextBlock(0.0, 0.4, 0.0, 0.0));
-
-        SolveWindows();
+        FGColor = ConsoleColor.White;
+        BGColor = ConsoleColor.Black;
     }
 
-    public void LeChonkRenderingMethod() //TODO: temporary name to avoid conflicts
+    public void SolveWindows(int SizeX, int SizeY) //TODO: throw exceptions for overlapping / out of bound / impossible screens
     {
-        SolveWindows();
-        Render();
-    }
-
-    //TODO: make private
-    public void SolveWindows() //TODO: throw exceptions for overlapping / out of bound / impossible screens
-    {
-        (SizeX, SizeY) = TermInfo.GetSize();
-
         //TODO: this math is off by one. It can lose one column and one row.
         foreach(var (key, val) in Windows)
         {
@@ -53,196 +37,4 @@ public class Screen
             window.aHeight = (int)(SizeY * window.Height);
         }
     }
-
-    //Renders all Windows
-    public void Render()
-    {
-        foreach(var (key, val) in Windows)
-            Render(val);
-    }
-
-    //Render specific Window by name
-    public void Render(string WinName)
-    {
-        Render(Windows[WinName]);
-    }
-
-    //Render given window
-    private void Render(Window Rendering) //TODO: if frame buffer somehow ends up with missing tokens then do an exception handler to just output them blank
-    {
-        Rendering.GenFrameBuffer();
-
-        //Set cursor to top left of window
-        //Move cursor along line by line printing character by character via it's frame buffer   
-        for(int y=0; y<Rendering.aHeight; y++)
-        {
-            Console.SetCursorPosition(Rendering.aXOffset,Rendering.aYOffset+y);
-            for(int x=0; x<Rendering.aWidth; x++)
-            {
-                Console.Write(Rendering.FrameBuffer[y][x].Text);
-            }
-        }
-    }
-
-    /*
-    //Renders all Windows
-    public void Render()
-    {
-        foreach(var (key, val) in Windows)
-            Render(val);
-    }
-    //Renders specific Window by name
-    public void Render(string name) {Render(Windows[name]);}
-
-    //Renders specific Window
-    public void Render(Window window)
-    {
-        int xo = window.xOffset;
-        int yo = window.yOffset;
-
-        for(int y=0; y<window.MinHeight; y++) //TODO: can this be a nested foreach? idk
-        {
-            for(int x=0; x<window.MinWidth; x++)
-            {
-                Console.SetCursorPosition(x+xo,y+yo);
-
-                Token token = window.FrameBuffer[y][x];
-
-                Console.ForegroundColor = token.Color;
-                Console.BackgroundColor = token.BGColor;
-                Console.Write(token.Text);
-            }
-        }
-    }
-
-    */
-
-    //Draws border of Everything
-    public void Display()
-    {
-        //Draw border Outline
-        DisplayScreen();
-        
-        //Cycle through Windows, displaying them one by one.
-        foreach(KeyValuePair<string, Window> entry in Windows)
-            DisplayWindow(entry.Value, true);
-    }
-
-    //Draws border of the Screen
-    public void DisplayScreen()
-    {
-        DisplayBox(0,0,SizeX,SizeY,false);
-    }
-
-    //Draws border of specific Window
-    private void DisplayWindow(Window window, bool bold)
-    {
-        int x = window.aXOffset;
-        int y = window.aYOffset;
-        int w = window.aWidth;
-        int h = window.aHeight;
-
-        DisplayBox(x, y, w, h, bold);
-    }
-
-    //Moves cursor to draw an arbitrarily sized border.
-    private void DisplayBox(int x, int y, int w, int h, bool bold)
-    {
-        char[] b; //Border lines
-
-        if(bold)
-            b = new char[] {'╔','╗','═','║','╚','╝'}; 
-        else
-            b = new char[] {'┌','┐','─','│','└','┘'};
-
-        /* Prints top row and right column
-         * Starting at top left corner.
-         * ​ ╔══╗
-         *     ║
-         */
-        Console.SetCursorPosition(x, y);
-        Console.Write(b[0]);
-        for(int i = 1; i < w-1; i++)
-            Console.Write(b[2]);
-
-        Console.Write(b[1]);
-        Console.SetCursorPosition(x+w-1, y+1);
-        for(int i = 1; i < h; i++)
-        {
-            Console.Write(b[3]);
-            Console.SetCursorPosition(x+w-1, y+i);
-        }
-
-        /* Prints left column and bottom row
-         * Starting again at top left corner.
-         *  ║
-         * ​ ╚══╝
-         */
-        Console.SetCursorPosition(x, y+1);
-        for(int i = 1; i < h; i++)
-        {
-            Console.Write(b[3]);
-            Console.SetCursorPosition(x, y+i);
-        }
-        Console.Write(b[4]);
-        for(int i = 1; i < w-1; i++)
-            Console.Write(b[2]);
-        Console.Write(b[5]);
-    }
 }
-
- /*\
-​╔═╦═╗
-║ ║ ║
-╠═╬═╣
-║ ║ ║
-╚═╩═╝
-┌─┬─┐
-│ │ │
-├─┼─┤
-│ │ │
-└─┴─┘
- \*/
-
-/* Code Graveyard *\
-
-    public void Display(int x, int y)
-    {
-        var box = new System.Text.StringBuilder("");
-
-        for(int i=0; i<y; i++)
-        {
-            char columnL;
-            char columnR;
-            char row;
-            if(i==0)
-            {
-                columnL = '┌';
-                columnR = '┐';
-                row = '─';
-            }
-            else if(i!=y-1)
-            {
-                columnL = '│';
-                columnR = '│';
-                row = ' ';
-            }
-            else //if(i==y-1)
-            {
-                columnL = '└';
-                columnR = '┘';
-                row = '─';
-            }
-
-            box.Append(columnL);
-            box.Append(row,x-2);
-            box.Append(columnR);
-
-            if(i<y-1)
-                box.AppendLine();
-        }
-        Console.Write(box);
-    }
-
-
-\* */
--- Termination/Terminal.cs
+++ Termination/Terminal.cs
@@ -2,23 +2,24 @@ namespace Termination;
 public class Terminal
 {
     
-    public Screen screen;
-    //private readonly ConsoleInput input = new();
-    public event EventHandler<ConsoleKeyInfo>? KeyPressed; //Event for when key is pressed
+    public Screen screen {get; set;}
 
+    public int SizeX, SizeY; //Size of terminal
+
+    public ConsoleColor DefFG = ConsoleColor.White; //User's default foreground color. TODO: derive from user's terminal at start
+    public ConsoleColor DefBG = ConsoleColor.Black; //User's default background color.
 
     public bool EnableInput {get; set;} = true; //Whether to recieve input at all
     public bool EchoInput {get; set;} = false; //Whether to print the user input
+    
+    public event EventHandler<ConsoleKeyInfo>? KeyPressed; //Event for when key is pressed
     private bool ShowDebug = false; //Display cursor and window boundaries?
 
     public Terminal()
     {
+        (SizeX, SizeY) = TermInfo.GetSize();
         KeyPressed += OnKeyPressed;
         screen = new();
-
-        screen.Windows["thr"].Data = "What the fuck did you just fucking say about me, you little bitch? I'll have you know I graduated top of my class in the Navy Seals, and I've been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I'm the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You're fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that's just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little \"clever\" comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn't, you didn't, and now you're paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You're fucking dead, kiddo.";
-        screen.Windows["one"].Data = "The FitnessGram Pacer test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter Pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal *boop*. A single lap should be completed each time you hear this sound *ding*. Remember to run in a straight line, and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready, start.";
-        screen.Windows["two"].Data = "I'd just like to interject for a moment. What you're refering to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX. Many computer users run a modified version of the GNU system every day, without realizing it. Through a peculiar turn of events, the version of GNU which is widely used today is often called Linux, and many of its users are not aware that it is basically the GNU system, developed by the GNU Project. There really is a Linux, and these people are using it, but it is just a part of the system they use. Linux is the kernel: the program in the system that allocates the machine\'s resources to the other programs that you run. The kernel is an essential part of an operating system, but useless by itself; it can only function in the context of a complete operating system. Linux is normally used in combination with the GNU operating system: the whole system is basically GNU with Linux added, or GNU/Linux. All the so-called Linux distributions are really distributions of GNU/Linux!";
     }
 
     public void Start()
@@ -40,7 +41,8 @@ public class Terminal
         }
     }
 
-    void Render() //TODO: make cross platform detector of window size change to automate rerendering
+    //Render Screen
+    private void Render() //TODO: make cross platform detector of window size change to automate rerendering
     {
         // while(true)
         // {
@@ -60,20 +62,43 @@ public class Terminal
         //             break;
         //     }
         // }
-
+        (SizeX, SizeY) = TermInfo.GetSize();
         Console.CursorVisible = ShowDebug;
         Console.Clear();
-        var (x, y) = TermInfo.GetSize();
-        screen.SolveWindows();
-        screen.Render();
-
+        screen.SolveWindows(SizeX,SizeY);
+        foreach(var (key, val) in screen.Windows)
+            RenderWindow(val);
+        
         if(ShowDebug)
         {
-            screen.Display();
+            Border();
         }
     }
 
-    void OnKeyPressed(object? sender, ConsoleKeyInfo key)
+    //Render given window
+    private void RenderWindow(Window Rendering) //TODO: if frame buffer somehow ends up with missing tokens then do an exception handler to just output them blank
+    {
+        Rendering.GenFrameBuffer();
+
+        //Set cursor to top left of window
+        //Move cursor along line by line printing character by character via it's frame buffer   
+        for(int y=0; y<Rendering.aHeight; y++)
+        {
+            Console.SetCursorPosition(Rendering.aXOffset,Rendering.aYOffset+y);
+            for(int x=0; x<Rendering.aWidth; x++)
+            {
+                var token = Rendering.FrameBuffer[y][x];
+                Console.ForegroundColor = token.Color;
+                Console.BackgroundColor = token.BGColor;
+                Console.Write(token.Text);
+            }
+        }
+
+        Console.ForegroundColor = screen.FGColor;
+        Console.BackgroundColor = screen.BGColor;
+    }
+
+    private void OnKeyPressed(object? sender, ConsoleKeyInfo key)
     {
         switch(key.Key)
         {
@@ -85,6 +110,7 @@ public class Terminal
 
             case ConsoleKey.X:
             {
+                CleanUp();
                 Environment.Exit(0);
                 break;
             }
@@ -96,5 +122,86 @@ public class Terminal
             }
 
         }
+    }
+
+    //Draws border of Everything
+    public void Border()
+    {
+        //Draw border Outline
+        BorderScreen();
+        
+        //Cycle through Windows, displaying them one by one.
+        foreach(KeyValuePair<string, Window> entry in screen.Windows)
+            BorderWindow(entry.Value, true);
+    }
+
+    //Draws border of the Screen
+    public void BorderScreen()
+    {
+        DrawBox(0,0,SizeX,SizeY,false);
+    }
+
+    //Draws border of specific Window
+    public void BorderWindow(Window window, bool bold)
+    {
+        int x = window.aXOffset;
+        int y = window.aYOffset;
+        int w = window.aWidth;
+        int h = window.aHeight;
+
+        DrawBox(x, y, w, h, bold);
+    }
+
+    //Moves cursor to draw an arbitrarily sized border.
+    private void DrawBox(int x, int y, int w, int h, bool bold)
+    {
+        char[] b; //Border lines
+
+        if(bold)
+            b = new char[] {'╔','╗','═','║','╚','╝'}; 
+        else
+            b = new char[] {'┌','┐','─','│','└','┘'};
+
+        /* Prints top row and right column
+         * Starting at top left corner.
+         * ​ ╔══╗
+         *     ║
+         */
+        Console.SetCursorPosition(x, y);
+        Console.Write(b[0]);
+        for(int i = 1; i < w-1; i++)
+            Console.Write(b[2]);
+
+        Console.Write(b[1]);
+        Console.SetCursorPosition(x+w-1, y+1);
+        for(int i = 1; i < h; i++)
+        {
+            Console.Write(b[3]);
+            Console.SetCursorPosition(x+w-1, y+i);
+        }
+
+        /* Prints left column and bottom row
+         * Starting again at top left corner.
+         *  ║
+         * ​ ╚══╝
+         */
+        Console.SetCursorPosition(x, y+1);
+        for(int i = 1; i < h; i++)
+        {
+            Console.Write(b[3]);
+            Console.SetCursorPosition(x, y+i);
+        }
+        Console.Write(b[4]);
+        for(int i = 1; i < w-1; i++)
+            Console.Write(b[2]);
+        Console.Write(b[5]);
+    }
+
+    private void CleanUp() //Cleans up terminal. Used before exiting
+    {
+        Console.ForegroundColor = DefFG;
+        Console.BackgroundColor = DefBG;
+        Console.CursorVisible = true;
+        Console.Clear();
     }
 }
